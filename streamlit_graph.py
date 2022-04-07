26632110deb11994822d39eaccd164f59838defc
MatFrancois <matthieu.francois@yahoo.fr>
2022-04-07 14:20:49 +0200

    maj cache and images

--- streamlit_graph.py
+++ streamlit_graph.py
@@ -20,14 +20,31 @@ from unidecode import unidecode
 # voisin3 | voisin3 | voisin3 | 
 # 
 
-# PRESENTATION
-
-
-
-
 DATA='/data/mfrancois/social_computing/leaders_community.json'
 MODELS='/data/mfrancois/social_computing/models'
-TEST='/data/mfrancois/social_computing/toto.json'
+
+st.set_page_config(
+ page_title="",
+ page_icon="🧊",
+ layout="wide",
+#  initial_sidebar_state="expanded",
+ menu_items={
+ 'Get Help': 'https://www.extremelycoolapp.com/help',
+ 'Report a bug': "https://www.extremelycoolapp.com/bug",
+ 'About': "# This is a header. This is an *extremely* cool app!"
+ }
+ )
+
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
 @st.cache(allow_output_mutation=True)
 def load_models(path):
     files = os.listdir(path)
@@ -56,14 +73,32 @@ def light_prepro(mot):
     """
     return unidecode(mot.lower().strip())
 
-@st.cache(allow_output_mutation=True)
+# @st.cache(allow_output_mutation=True)
 def get_similar_words(word, model, n):
     return {m: pd.DataFrame(list(map(lambda x: light_prepro(x[0]), v.wv.similar_by_word(word, topn=n))), columns=['Neighbors']) for m, v in model.items() if word in v.wv.key_to_index}
-    
+
+def leaders_to_df(community_details, cluster_id):
+    # print(community_details)
+    df = pd.DataFrame.from_dict(community_details.get(cluster_id), orient='index')
+    df['username'] = df.index
+    return df[['username', 'edges']].sort_values(by='edges', ascending=False)
+ 
+ 
 print('starting')
 
-keyword = st.text_input(label='Choose keyword',value='climat')
-n_voisins = st.number_input('Choose Number of neighbors', min_value=3, max_value=30, value=10)
+_, col, _ = st.columns(3)
+col.title("App Title")
+col.markdown(
+    ''' > *Présentation des la méthode de formation de communauté et de l'entrainement de n word2vec pour n communauté*
+    '''
+)
+col.image('images/louvain_algo.png')
+keyword = col.selectbox(label="allowed keyword", options=('nature', 'cop26', 'nucléaire', 'eolien', 'climat', 'musulman')) # prend comme value la première option
+# keyword = col.text_input(label='Choose keyword',value='climat')
+# n_voisins = col.number_input('Choose Number of neighbors', min_value=3, max_value=30, value=10)
+
+n_voisins = col.slider('Number of neighbors to display',0, 30, value=10)
+# my_bar = st.progress(0)
 
 print(f'n_voisins   :       {n_voisins}')
 
@@ -80,34 +115,29 @@ if keyword:
 
     st.title(keyword)
 
-    compteur = 1
-    while compteur != len(models):
+    compteur = 0
+    while compteur < len(community_details):
         
         c = st.container()
-        n_col = min(len(models)-compteur, 3)
+        print(f'longueur des communautés : {len(community_details)}')
+        n_col = min(len(community_details)-compteur, 5)
 
         col = c.columns(n_col)
 
         for l, co in enumerate(col):
-            j = list(models.keys())[compteur-l-1]
-            title = f'community {j}'
-            with co.expander(title, expanded=True):
-                # buttons[j] = st.button('know more', key=str(j))
-                st.table(sim_dict.get(j))
+            j = list(community_details.keys())[compteur-l-1] # à remplacer par l'ordre d'apparition des leaders
+            title = f'Community {j}'
+            
+            # display leaders
+            co.subheader(title)
+            with co.expander('leaders'):
+                st.table(leaders_to_df(community_details, j))
+            # co.markdown("""---""")
+            co.table(sim_dict.get(j))
+        
+        st.markdown("""---""")
+        compteur += 5
 
-        compteur += 3            
-
-# for key, val in buttons.items():
-#     if val:
-#         print(f'community n°{key} details asked')
-#         st.sidebar.text(community_details.get(key))
-side = st.sidebar.container()
-side.select_slider()
-# leaders = {}
-# for k, v in buttons.items():
-#     if v:
-#         leaders[v] = get_community(community_details, cluster_id=k, nb_user=5)
-#         st.sidebar.text(leaders.get(v))
 
 print('fini')
 
